1246f7fdd32ecc29a259db1812b15222e5d7e466
Osalotioman <osalotioman@gmail.com>
2026-03-19 07:25:22 +0100

    chore: Add prisma schema

--- api-server/src/db/prisma.ts
+++ api-server/src/db/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/client";
 
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL ?? "",
